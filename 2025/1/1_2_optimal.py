f18f4ed00e2db142e87f5fb8a71b26a51d502fed
Puermuta <19885813+Puermuta@users.noreply.github.com>
2025-12-01 22:52:36 +0100

    Added O(1) solution

---
+++ 2025/1/1_2_optimal.py
@@ -0,0 +1,22 @@
+path = "./data.txt"
+
+dial = 50
+result = 0
+rights = 0
+lefts = 0
+
+
+with open(path, "r") as file:
+    for line in file:
+        if line[1:] == "": continue
+
+        move = int(line[1:])
+
+        if line[0] == "R":
+            result += (dial + move) // 100
+            dial = (dial + move) % 100
+        else:
+            result += ((100 - dial)%100 + move) // 100
+            dial = (dial - move) % 100
+
+print(result)
